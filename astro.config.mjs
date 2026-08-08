87c2ddae45c59b1dba51eff87dd9f6312ae8e3df
Marisol Henao <marisolhenao.dev@gmail.com>
2026-08-08 17:07:35 -0500

    chore: add pnpm workspace configuration and TypeScript settings
    - Created pnpm-workspace.yaml to allow builds for esbuild and sharp.
    - Added tsconfig.json extending Astro's strict configuration with custom paths and include/exclude settings.

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import mdx from "@astrojs/mdx";
+
+const prod = process.env.NODE_ENV === "production";
+const repoName = "proyecto-abp";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
+      },
+    },
+  },
+  integrations: [mdx()],
+  site: "https://margin-zero-important.github.io",
+  base: prod ? `/${repoName}` : "/",
+});
